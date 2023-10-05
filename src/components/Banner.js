d6040361d703c497f5132391a88bc8ad7f172f64
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-06 02:26:20 +0900

    Banner UI생성하고 CSS 스타일링

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../api/axios';
 import requests from './../api/requests';
+import "./Banner.css"
 
 
 export default function Banner() {
@@ -13,20 +14,47 @@ export default function Banner() {
 
         //현재 상영중인 영화 정보를 가져오기(여러 영화)
         const request = await axios.get(requests.fetchNowPlaying);
-        
+
         //여러 영화 중 영화 하나의 ID를 가져오기
         const movieID = request.data.results[
             Math.floor(Math.random() * request.data.results.length)
         ].id;
 
         //특정 영화의 더 상세한 정보를 가져오기(비디오정보도 포함)
-        const {data: movieDetail} = await axios.get(`movie/${movieID}`, {
-            params: {append_to_response: "videos"}
+        const { data: movieDetail } = await axios.get(`movie/${movieID}`, {
+            params: { append_to_response: "videos" }
         })
         setMovie(movieDetail);
     };
 
-    return (
-        <div></div>
-    )
+    return <header
+        className="banner"
+        style={{
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+            backgroundPosition: "top center",
+            backgroundSize: "cover",
+        }}
+    >
+        <div className="banner__contents">
+            <h1 className="banner__title">
+                {movie.title || movie.name || movie.original_name}
+            </h1>
+
+            <div className="banner__buttons">
+                <button
+                    className="banner__button play"
+                >
+                    Play
+                </button>
+                <button className="banner__button info">More Information</button>
+            </div>
+
+            <h1 className="banner__description">
+                {movie.overview}
+            </h1>
+        </div>
+        <div className="banner--fadeBottom" />
+    </header>
+
+
 }
