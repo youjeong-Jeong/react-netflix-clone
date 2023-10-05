2cbe9f0165c87a41f0afdcecda358951fa2b0a2d
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-06 03:32:33 +0900

    Row 컴포넌트 생성 후 APP에 가져오기

--- src/api/requests.js
+++ src/api/requests.js
@@ -2,6 +2,7 @@ const requests = {
     fetchNowPlaying: `movie/now_playing`,
     fetchNetflixOriginals: `/discover/tv?with_networks=213`,
     fetchTrending: `/trending//all/week`,
+    fetchTopRated: "/movie/top_rated",
     fetchActionMovies: "/discover/movie?with_genres=28",
     fetchComedyMovies: "/discover/movie?with_genres=35",
     fetchHorrorMovies: "/discover/movie?with_genres=27",
