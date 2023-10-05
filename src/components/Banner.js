25185a8cbe9ec0e1703b5f6592d93ac0a60e2b93
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-06 03:18:36 +0900

    Styled Component를 이용해서 비디오 배너 생성
    play 버튼을 눌렀을 때 해당 영화의 유튜브 티져 영상을 보여줄 수 있게 함

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import axios from '../api/axios';
 import requests from './../api/requests';
 import "./Banner.css"
+import styled from 'styled-components';
 
 
 export default function Banner() {
     const [movie, setMovie] = useState([]);
+    const [isClicked, setisClicked] = useState(false);
+
     useEffect(() => {
         fetchData();
     }, [])
@@ -29,37 +32,84 @@ export default function Banner() {
 
     //설명 글 100자 이상이면 자른 후 ...붙여주는 함수
     const truncate = (str, n) => {
-        return str?.length > n ? str.substr(0, n-1) + "..." : str;
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
     }
 
-    return <header
-        className="banner"
-        style={{
-            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-            backgroundPosition: "top center",
-            backgroundSize: "cover",
-        }}
-    >
-        <div className="banner__contents">
-            <h1 className="banner__title">
-                {movie.title || movie.name || movie.original_name}
-            </h1>
+    if (!isClicked) {
+        return <header
+            className="banner"
+            style={{
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                backgroundPosition: "top center",
+                backgroundSize: "cover",
+            }}
+        >
+            <div className="banner__contents">
+                <h1 className="banner__title">
+                    {movie.title || movie.name || movie.original_name}
+                </h1>
 
-            <div className="banner__buttons">
-                <button
-                    className="banner__button play"
-                >
-                    Play
-                </button>
-                <button className="banner__button info">More Information</button>
+                <div className="banner__buttons">
+                    <button
+                        className="banner__button play"
+                        onClick={() => setisClicked(true)}
+                    >
+                        Play
+                    </button>
+                    <button className="banner__button info">More Information</button>
+                </div>
+
+                <h1 className="banner__description">
+                    {truncate(movie.overview, 100)}
+                </h1>
             </div>
-
-            <h1 className="banner__description">
-                {truncate(movie.overview, 100)}
-            </h1>
-        </div>
-        <div className="banner--fadeBottom" />
-    </header>
-
-
+            <div className="banner--fadeBottom" />
+        </header>
+    } else {
+        return (
+            <Container>
+                <HomeContainer>
+                    <Iframe
+                    width="640"
+                    height="360"
+                    src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen></Iframe>
+                </HomeContainer>
+            </Container>
+        )
+    }
 }
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`;
+
+const Iframe = styled.iframe`
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    //opacity: 0.65;
+    border: none;
+
+    &::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+`;
