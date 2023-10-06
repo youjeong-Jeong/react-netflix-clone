662cd9f5be81749e2f650d5e443393568847343c
youjeong Jeong <wjddbwjd1205@naver.com>
2023-10-06 14:35:30 +0900

    검색 페이지 구현
    NavBar에 검색 Input 생성,
    Search 페이지에서 SearchTerm 가져오기,
    SearchTerm이 바뀔 때마다 새로 영화 데이터 가져오기

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState} from 'react'
 import "./Nav.css"
+import { useNavigate } from 'react-router-dom';
 
 export default function Nav() {
 
     const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState("")
+    const navigate = useNavigate();
+
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -19,6 +23,11 @@ export default function Nav() {
         };
     }, []);
 
+    const handleChange = (e) => {
+        setSearchValue(e.target.value)
+        navigate(`/search?q=${e.target.value}`)
+    };
+
     return <nav className={`nav ${show && "nav__black" }`}>
         <img
             alt="Netflix logo"
@@ -26,6 +35,14 @@ export default function Nav() {
             className="nav__logo"
             onClick={() => (window.location.href = "/")}
         />
+
+        <input 
+            value={searchValue} 
+            onChange={handleChange} className='nav__input' 
+            type="text"
+            placeholder='영화를 검색해주세요'
+            />
+
         <img
             alt="User logged"
             src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
---
+++ src/pages/SearchPage/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import axios from '../../api/axios';
+
+export default function SearchPage() {
+
+    const [searchResults, setSearchResults] = useState([]);
+    const useQuery = () => {
+        return new URLSearchParams(useLocation().search)
+    }
+    let query = useQuery();
+    const searchTerm = query.get("q")
+
+    useEffect(() => {
+        if (searchTerm) {
+            fetchSearchMovie(searchTerm);
+        }
+    }, [searchTerm])
+
+    const fetchSearchMovie = async (searchTerm) => {
+        try {
+            const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+            console.log(request);
+            setSearchResults(request.data.results)
+;        } catch (error) {
+            console.log("error", error)
+        }
+    }
+
+
+    return (
+        <div>index</div>
+    )
+}
